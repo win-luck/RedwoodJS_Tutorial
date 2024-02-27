2f4b6c5769fb0d4b05d62e5e00d5909665b028cd
CHAE <chaso1495@naver.com>
2024-02-28 00:29:36 +0900

    [feat] Jest 관련 컴포넌트 수정

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -2,20 +2,26 @@ import { Link, routes } from '@redwoodjs/router'
 
 import type { Post } from 'types/graphql'
 
-interface Props { // 마찬가지로 인터페이스로 Props를 정의한다.
-  article: Post
+const truncate = (text: string, length: number) => {
+  return text?.substring(0, length) + '...'
 }
 
-const Article = ({ article }: Props) => {
+interface Props {
+  article: Omit<Post, 'createdAt'>
+  summary?: boolean
+}
+
+const Article = ({ article, summary = false }: Props) => {
   return (
-    <article>
+    <article className="mt-10">
       <header>
-        <h2>
+        <h2 className="text-xl text-blue-700 font-semibold">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div>{article.body}</div>
-      <div>Posted at: {article.createdAt}</div>
+      <div className="mt-2 text-gray-900 font-light">
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
     </article>
   )
 }
