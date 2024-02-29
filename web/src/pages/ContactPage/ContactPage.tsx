329b635b1de16a7925a9a0da022db9bc718d5d02
CHAE <chaso1495@naver.com>
2024-02-29 20:19:22 +0900

    [fix] 기존 UI 수정 및 테일윈드 적용

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,20 +1,16 @@
-import {Metadata, useMutation} from '@redwoodjs/web'
-import { toast, Toaster } from '@redwoodjs/web/toast'
+import { Metadata } from '@redwoodjs/web'
 import {
-  FieldError,
   Form,
-  FormError,
   TextField,
   TextAreaField,
-  useForm,
   Submit,
-  SubmitHandler, Label
+  FieldError,
+  Label,
+  FormError,
+  useForm,
 } from '@redwoodjs/forms'
-
-import {
-  CreateContactMutation,
-  CreateContactMutationVariables,
-} from 'types/graphql'
+import { useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
 
 const CREATE_CONTACT = gql`
   mutation CreateContactMutation($input: CreateContactInput!) {
@@ -24,67 +20,95 @@ const CREATE_CONTACT = gql`
   }
 `
 
-interface FormValues {
-  name: string
-  email: string
-  message: string
-}
-
 const ContactPage = () => {
   const formMethods = useForm()
-  const [create, { loading, error }] = useMutation<
-    CreateContactMutation,
-    CreateContactMutationVariables
-  >(CREATE_CONTACT, {
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
     onCompleted: () => {
       toast.success('Thank you for your submission!')
       formMethods.reset()
     },
   })
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    create({variables: { input: data} })
+  const onSubmit = (data) => {
+    create({ variables: { input: data } })
+    console.log(data)
   }
 
   return (
     <>
-      <Metadata title="Contact" description="Contact page"/>
+      <Metadata title="Contact" description="Contact page" />
 
       <Toaster />
-      <Form onSubmit={onSubmit} config={{mode: 'onBlur'}} error={error} formMethods={formMethods}>
-        <FormError error={error} wrapperClassName={"form-error"}/>
-        <Label name="name" errorClassName="error">
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperClassName="py-4 px-6 rounded-lg bg-red-100 text-red-700"
+          listClassName="list-disc ml-4"
+          listItemClassName=""
+        />
+        <Label
+          name="name"
+          className="block text-gray-700 uppercase text-sm"
+          errorClassName="block uppercase text-sm text-red-700"
+        >
           Name
         </Label>
         <TextField
           name="name"
-          validation={{required: true}}
-          errorClassName="error"
+          validation={{ required: true }}
+          className="border rounded-sm px-2 py-1 outline-none"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
         />
-        <FieldError name="name" className="error"/>
+        <FieldError name="name" className="block text-red-700" />
 
-        <Label name="email" errorClassName="error">
+        <Label
+          name="email"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
           Email
         </Label>
         <TextField
           name="email"
           validation={{
-            required: true
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
           }}
-          errorClassName="error"
+          className="border rounded-sm px-2 py-1"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
         />
-        <FieldError name="email" className="error"/>
+        <FieldError name="email" className="block text-red-700" />
 
-        <Label name="message" errorClassName="error">
+        <Label
+          name="message"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
           Message
         </Label>
         <TextAreaField
           name="message"
-          validation={{required: true}}
-          errorClassName="error"
+          validation={{ required: true }}
+          className="block border rounded-sm px-2 py-1"
+          errorClassName="block border rounded-sm px-2 py-1 border-red-700 outline-none"
         />
-        <FieldError name="message" className="error"/>
-        <Submit disabled={loading}>Save</Submit>
+        <FieldError name="message" className="block text-red-700" />
+
+        <Submit
+          className="block bg-blue-700 text-white mt-8 px-4 py-2 rounded"
+          disabled={loading}
+        >
+          Save
+        </Submit>
       </Form>
     </>
   )
