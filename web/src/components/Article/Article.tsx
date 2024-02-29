897562ba7f93164c8b2e36659d4aae33c0c5a528
CHAE <chaso1495@naver.com>
2024-02-29 20:20:21 +0900

    [feat] Article에 CommentCell 연결 및 테스트코드 작성

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,9 +1,10 @@
 import { Link, routes } from '@redwoodjs/router'
+import CommentsCell from 'src/components/CommentsCell'
 
 import type { Post } from 'types/graphql'
 
 const truncate = (text: string, length: number) => {
-  return text?.substring(0, length) + '...'
+  return text.substring(0, length) + '...'
 }
 
 interface Props {
@@ -13,7 +14,7 @@ interface Props {
 
 const Article = ({ article, summary = false }: Props) => {
   return (
-    <article className="mt-10">
+    <article>
       <header>
         <h2 className="text-xl text-blue-700 font-semibold">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
@@ -22,6 +23,11 @@ const Article = ({ article, summary = false }: Props) => {
       <div className="mt-2 text-gray-900 font-light">
         {summary ? truncate(article.body, 100) : article.body}
       </div>
+      {!summary && (
+        <div className="mt-12">
+          <CommentsCell />
+        </div>
+      )}
     </article>
   )
 }
