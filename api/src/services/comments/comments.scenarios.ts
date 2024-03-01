113fca2e5c39b859754a4149aecd6ff17e69b5a3
CHAE <chaso1495@naver.com>
2024-03-01 16:14:51 +0900

    [fix] comments.scenarios user 관계 반영하여 수정

--- api/src/services/comments/comments.scenarios.ts
+++ api/src/services/comments/comments.scenarios.ts
@@ -9,7 +9,14 @@ export const standard = defineScenario<Prisma.CommentCreateArgs>({
         post: {
           create: {
             title: 'Redwood Leaves',
-            body: 'The quick brown fox jumped over the lazy dog.'
+            body: 'The quick brown fox jumped over the lazy dog.',
+            user: {
+              create: {
+                email: "jane@naver.com",
+                hashedPassword: "awdiwjflwkajwwkfw",
+                salt: "asdaswd"
+              }
+            }
           }
         }
       }
@@ -22,6 +29,13 @@ export const standard = defineScenario<Prisma.CommentCreateArgs>({
           create: {
             title: 'Root Systems',
             body: 'The five boxing wizards jump quickly.',
+            user: {
+              create: {
+                email: "john@naver.com",
+                hashedPassword: "asdaw",
+                salt: "awdwqfq"
+              }
+            }
           }
         }
       }
@@ -35,6 +49,13 @@ export const postOnly = defineScenario<Prisma.PostCreateArgs>({
       data: {
         title: 'Bark',
         body: "A tree's bark is worse than its bite",
+        user: {
+          create: {
+            email: "",
+            hashedPassword: "",
+            salt: ""
+          }
+        }
       }
     }
   }
