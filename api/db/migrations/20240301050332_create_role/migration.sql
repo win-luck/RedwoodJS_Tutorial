c2e4814eec4a5ddb558ae5b18959e38924a99217
CHAE <chaso1495@naver.com>
2024-03-01 16:16:39 +0900

    [chore] sql 마이그레이션 반영

---
+++ api/db/migrations/20240301050332_create_role/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `roles` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "webAuthnChallenge" TEXT,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME,
+    "roles" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "hashedPassword", "id", "name", "resetToken", "resetTokenExpiresAt", "salt", "webAuthnChallenge") SELECT "email", "hashedPassword", "id", "name", "resetToken", "resetTokenExpiresAt", "salt", "webAuthnChallenge" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
